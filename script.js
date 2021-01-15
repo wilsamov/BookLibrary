a6fe9d44291005ba4bf65b1a03b819f615cb881a
wilsamov <wilsamov@gmail.com>
2021-01-15 11:13:44 -0800

    First Commit

---
+++ script.js
@@ -0,0 +1,12 @@
+let myLibrary = [];
+
+function book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+}
+
+function addBookToLibrary() {
+
+}
